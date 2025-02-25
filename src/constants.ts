7101512c24c40589393b13f2b10c86c8dc839ad6
Jose Salazar <jose.salazar@webjet.com.au>
2025-02-26 07:06:24 +1100

    config fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL_DEV = 'http://localhost:9090/api';
-export const BACKEND_URL_PROD = 'https://sowebjet-a6crdfc4dzh2hver.australiasoutheast-01.azurewebsites.net//api';
-export const BACKEND_URL = 'https://sowebjet-a6crdfc4dzh2hver.australiasoutheast-01.azurewebsites.net/';
+export const BACKEND_URL_PROD = 'https://sowebjet-a6crdfc4dzh2hver.australiasoutheast-01.azurewebsites.net/api';
+export const BACKEND_URL = 'https://sowebjet-a6crdfc4dzh2hver.australiasoutheast-01.azurewebsites.net/api';
